699edac4c9d51d9b38fa00c196714d5a726a37b2
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-05-23 00:16:48 -0700

    trying to get git workng

--- Shipwrecked/js/Shipwrecked.js
+++ Shipwrecked/js/Shipwrecked.js
@@ -29,10 +29,6 @@ class Shipwrecked extends Phaser.Scene {
 
                 // plugins
                 this.load.plugin('DialogModalPlugin', './js/dialog_plugin.js');
-                this.sys.install('DialogModalPlugin');
-                //console.log(this.sys.dialogModal);
-                this.load.plugin('GlobalFunctionsPlugin', '.js/GlobalFunctions.js"');
-                this.sys.install('GlobalFunctionsPlugin');
 
                 // main images
                 this.load.image("bigSand", "assets/island_sand_d.jpg");
@@ -450,9 +446,8 @@ class Shipwrecked extends Phaser.Scene {
                 //this.scoreText = this.add.text(16, 16, myItem, { fontSize: "32px", fill: "#000" });
 
                 // adds header
-                //this.goldtext = this.add.text(this.sys.globalFunctions.goldTextFunction());
-                // this.goldText = this.add.text(20, 10, "Gold: " + Gold, { fontsize: "32px", strokeThickness: 1, stroke: "#fe0", fill: "#fe0", shadowStroke: true, shadowFill: true, shadowColor: "#000", shadowOffsetX: 1, shadowOffsetY: 1, align: "center" });
-                // this.goldText.setScrollFactor(0);
+                this.goldText = this.add.text(20, 10, "Gold: " + Gold, { fontsize: "32px", strokeThickness: 1, stroke: "#fe0", fill: "#fe0", shadowStroke: true, shadowFill: true, shadowColor: "#000", shadowOffsetX: 1, shadowOffsetY: 1, align: "center" });
+                this.goldText.setScrollFactor(0);
 
                 this.woodText = this.add.text(100, 10, "Wood: " + Wood, { fontsize: "32px", strokeThickness: 1, stroke: "#fe0", fill: "#fe0", align: "center" });
                 this.woodText.setScrollFactor(0);
@@ -479,7 +474,8 @@ class Shipwrecked extends Phaser.Scene {
                 //plugins
 
                 // Dialog box:
-
+                this.sys.install('DialogModalPlugin');
+                console.log(this.sys.dialogModal);
 
                 //this.dialogBox = this.add.text(200, 200, this.sys.dialogModal.init());
                 this.dialogBox = this.sys.dialogModal;
