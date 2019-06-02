8e9ac476bb244cb7f76a38638d07e49894c4d75d
Caryswebsite1 <46387153+Caryswebsite1@users.noreply.github.com>
2019-06-01 20:15:34 -0700

    map 4 complete with plugin text fix.

--- Shipwrecked/js/game.js
+++ Shipwrecked/js/game.js
@@ -1,24 +1,26 @@
+
+
 // our game's configuration
 // note the width and height are just the viewport sizes. 
 // they don't have to be the "world" sizes.  See setBounds..
 let config = {
-    type: Phaser.AUTO,
-    width: 1000,
-    height: 1000,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-
-    scene: [Shipwrecked, Shipwrecked2, Shipwrecked3, Shipwrecked4, ]
+  type: Phaser.AUTO,
+  width: 1000,
+  height: 1000,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+ 
+    scene: [Shipwrecked, Shipwrecked2, Shipwrecked3, Shipwrecked4,]
 };
 
 console.log("in game.js");
-let playerStartX = 500;
-let playerStartY = 500;
+let playerStartX = 350;
+let playerStartY = 400;
 // scene sleep checks
 let sleep1 = false;
 let sleep2 = false;
@@ -35,13 +37,16 @@ let playerInventory = [];
 // items
 let Gold = 0;
 let Wood = 0;
-let Rope = 0;
+let Iron = 0;
 let Wool = 0;
 let Food = 0;
 
 
-// tools
+// tools  Not sure we need these since we have the inventory.
 let Machete = "Machete";
+let Axe = "Axe";
 
 // create the game, and pass it the configuration
 let game = new Phaser.Game(config);
+
+
